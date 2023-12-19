7883a0b7c2b4b340b37bdb71f06a62d800de02fc
Acemage01 <acheampongb17@hotmail.com>
2023-12-18 16:40:09 -0800

    C projects: 0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+i#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int d, e, f;
+
+	for (d = 48; d < 58; d++)
+	{
+		for (e = 49; e < 58; e++)
+		{
+			for (f = 50; f < 58; f++)
+			{
+				if (f > e && e > d)
+				{
+					putchar(d);
+					putchar(e);
+					putchar(f);
+					if (d != 55 || e != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

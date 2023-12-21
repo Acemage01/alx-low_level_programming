f431187a942b81023bf73db969d18efa93e923b0
Acemage01 <acheampongb17@hotmail.com>
2023-12-20 20:05:35 -0800

    C projects: 0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints the sum of even-valued Fibonacci sequence terms not exceeding 4000000.
+ * main - Prints the sum of even-valued 
+ * Fibonacci sequence terms not exceeding 4000000.
  *
  * Return: 0 Always
  */
